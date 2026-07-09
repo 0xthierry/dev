ddb168633f14a9ec6b1897c2cbac2e3fd2921e78
Thierry Santos <thierrysantoos123@gmail.com>
2026-07-09 18:03:32 -0300

    chore(herdr): bump to 0.7.3

--- install/mise.sh
+++ install/mise.sh
@@ -23,7 +23,7 @@ java = "temurin-17.0.19+10"
 dotnet = "8.0.420"
 zig = "latest"
 aws = "latest"
-"github:ogulcancelik/herdr" = "0.7.1"
+"github:ogulcancelik/herdr" = "0.7.3"
 
 [settings]
 experimental = false
