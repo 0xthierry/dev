84c98ad56aeb0d1f13ee09a54272a6fbac5e7132
Thierry Santos <thierrysantoos123@gmail.com>
2026-07-21 20:07:52 -0300

    chore(pi): remove goal extension

--- configs/agents/pi/extensions/goal/index.ts
+++
@@ -1,6 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import { registerGoalExtension } from "./lib/register";
-
-export default function (pi: ExtensionAPI) {
-  registerGoalExtension(pi);
-}
