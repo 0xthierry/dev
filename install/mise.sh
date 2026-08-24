c3adcd85486eb15f88d2314709b910f63cd2e373
Thierry Santos <thierrysantoos123@gmail.com>
2026-08-24 09:47:41 -0300

    chore(pi): bump to 0.84.3

--- install/mise.sh
+++ install/mise.sh
@@ -15,7 +15,7 @@ write_mise_config() {
   cat > "$tmp" <<'EOF'
 [tools]
 node = "latest"
-"npm:@earendil-works/pi-coding-agent" = "0.84.2"
+"npm:@earendil-works/pi-coding-agent" = "0.84.3"
 pnpm = "latest"
 python = "3.12"
 go = "latest"
